4b7a3630b150b207206c8b5306f29fbdefdc3575
Tim Healey <thealey.1997@gmail.com>
2020-08-27 16:27:21 +0930

    comments added to CTCI: 1-6 String Compression

--- CrackingTheCodingInterview/1-6_StringCompression.cpp
+++ CrackingTheCodingInterview/1-6_StringCompression.cpp
@@ -28,10 +28,13 @@ string StringCompression(string in){
 	string sstr;
 
 	for (int i = 1; i < in.size(); i++){
-
+		// if the char matches, add to counter
 		if (in[i] == current){
 			counter++;
 		}
+		// otherwise we reset the counter and set the new char
+		// while appending the values being reset to the return string
+		// stringstream was used here, but to_string() of the std lib would work given you are using c++11
 		else{
 			ret += current;
 			ss << counter;
@@ -42,12 +45,13 @@ string StringCompression(string in){
 			counter = 1;
 		}
 	}
-
+	// add the final values since the loop breaks before doing so
 	ret += current;
 	ss << counter;
 	sstr = ss.str();
 	ret += sstr;
 
+	// return the shorter of the input or the compressed version
 	if (ret.size() < in.size()){
 		return ret;
 	}
